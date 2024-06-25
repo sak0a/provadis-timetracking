ad9a89e34841c892da0308a3e1d540515b69347e
Anton <klimmster1@gmail.com>
2024-06-25 13:51:57 +0200

    es wurden die dateien connection.php, crypt, log-in, logic, login.php hochgeladen

--- admin/pages/samples/connection.php
+++ admin/pages/samples/connection.php
@@ -3,10 +3,6 @@ $servername = "korra.design";
 $username = "provadis";
 $password = "alexandros2406";
 $dbname = "provadis_project";
-// $servername = "mysql34.1blu.de";
-// $username = "s319321_3591318";
-// $password = "HdJ%uxh0JogiZQk";
-// $dbname = "db319321x3591318";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
---
+++ admin/pages/samples/crypt.php
@@ -0,0 +1,21 @@
+<?php
+
+function encryptCookie($value){
+    $key = 'BcuIght/79AqsÜ??ßßnbgthrj-;'; // Ändern Sie dies zu einem sicheren Schlüssel
+    $cipher = "aes-256-cbc";
+    $ivlen = openssl_cipher_iv_length($cipher);
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $encrypted = openssl_encrypt($value, $cipher, $key, 0, $iv);
+    return base64_encode($encrypted . '::' . $iv);
+}
+function decryptCookie($cookie){
+    $key = 'BcuIght/79AqsÜ??ßßnbgthrj-;'; // Verwenden Sie denselben Schlüssel wie beim Verschlüsseln
+    list($encrypted_data, $iv) = explode('::', base64_decode($cookie), 2);
+    return openssl_decrypt($encrypted_data, "aes-256-cbc", $key, 0, $iv);
+}
+
+//$decryptCookid = decryptCookie($encryptedCookie);
+
+//setcookie('secure_user123', $encryptedCookie, time() + 3600*24, '/', '', true, true);
+
+?>
