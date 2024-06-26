a4e585d2598aff7dfecbc0724a7eb622423ac742
Anton <klimmster1@gmail.com>
2024-06-26 10:47:42 +0200

    die startseite wurde angepasst

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,71 @@
-
 <?php
 use vendor\database\Database;
 include("../backend/database/Database.php");
-
 session_start();
 
+if (!isset($_SESSION['angemeldet']) || $_SESSION['angemeldet'] !== true) {
+    header("Location: login.php");
+    exit();
+}
+
 $currentTab = "dashboard";
+$benutzerFirstName = htmlspecialchars($_SESSION['first_name']);
+$benutzerLastName = htmlspecialchars($_SESSION['last_name']);
+$benutzerEmail = htmlspecialchars($_SESSION['email']);
+$benutzerRole = htmlspecialchars($_SESSION['role']);
+
+
+if (isset($_POST['ajax']) && $_POST['ajax'] == '1') {
+    $ajaxFunction = $_POST['f'] ?? '';
+    if ($ajaxFunction == 'changeTab') {
+        $newTab = $_POST['tab'] ?? 'dashboard';
+        switch ($newTab) {
+            case "dashboard":
+            case "statistics":
+            case "projects":
+            case "employees":
+                $currentTab = $newTab;
+            default:
+                $currentTab = "dashboard";
+        }
+    }
+}
+
+
+
+// Logout-Logik
+if (isset($_POST['logout'])) {
+    unset($_SESSION['angemeldet']);
+    unset($_SESSION['username']);
+    if (isset($_COOKIE['secure_user'])) {
+        setcookie('secure_user', '', time() - 3600 * 24 * 7, '/');
+        unset($_COOKIE['secure_user']);
+    }
+    session_regenerate_id(true);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+if (isset($_POST['login']) && !empty($_POST['username'])) {
+    $_SESSION['angemeldet'] = true;
+    $_SESSION['username'] = $_POST['username'];
+    session_regenerate_id(true);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+if (isset($_COOKIE['secure_user']) && $_SESSION['angemeldet'] !== true) {
+    require_once 'crypt.php';
+    if (function_exists('decryptCookie')) {
+        $userId = decryptCookie($_COOKIE['secure_user']);
+        if ($userId) {
+            $_SESSION['angemeldet'] = true;
+            $_SESSION['username'] = $userId;
+            session_regenerate_id(true);
+            header("Location: " . $_SERVER['PHP_SELF']);
+            exit;
+        }
+    }
+}
 
 if (!isset($_SESSION['admin__current_tab'])) {
     $_SESSION['admin__current_tab'] = 'dashboard';
@@ -69,139 +129,157 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['ajax'])) {
     echo $tabContent;
     exit;
 }
-
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CommerzBau</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="shortcut icon" href="../assets/images/favicon.png" />
 </head>
+
 <body>
-
-<script src="../assets/js/preloader.js"></script>
-<script src="../assets/js/admin.js"></script>
-
+    <script src="../assets/js/preloader.js"></script>
+    <script src="../assets/js/admin.js"></script>
 
 
-<div class="body-wrapper">
-    <!-- partial:../../partials/_sidebar.html -->
-<aside class="mdc-drawer mdc-drawer--open">
-        <div class="mdc-drawer__header ">
-            <img src="../assets/images/logo.png"  width="200" alt="logo" class="pt-4">
-        </div>
-        <div class="mdc-drawer__content">
-            <div class="user-info">
-                <p class="name">Laurin Noel Frank</p>
-                <!-- TDOO: PHP trim email to n maximum characters -->
-                <p class="email">laurinnoel.frank@commer...</p>
+
+    <div class="body-wrapper">
+        <!-- partial:../../partials/_sidebar.html -->
+        <aside class="mdc-drawer mdc-drawer--open">
+            <div class="mdc-drawer__header ">
+                <img src="../assets/images/logo.png" width="200" alt="logo" class="pt-4">
             </div>
-            <div class="mdc-list-group">
-                <nav class="mdc-list mdc-drawer-menu">
-                    <div class="mdc-list-item mdc-drawer-item">
+            <div class="mdc-drawer__content">
+                <div class="user-info">
+                    <?php if (isset($_SESSION['angemeldet']) && $_SESSION['angemeldet'] === true) {
+                    ?>
+                        <div id="angemeldet_als"><?php echo $benutzerFirstName . ' ' . $benutzerLastName; ?></div>
+                    <?php } else { ?>
+                    <?php } ?>
+                    <!-- TDOO: PHP trim email to n maximum characters -->
+                    <p class="email"><?php if (isset($_SESSION['angemeldet']) && $_SESSION['angemeldet'] === true) {
+                                        ?>
+                    <div id="angemeldet_als"><?php echo $benutzerEmail; ?></div><?php } else { ?>
+                <?php } ?>
+                </p>
+                </div>
+                <div class="mdc-list-group">
+                    <nav class="mdc-list mdc-drawer-menu">
+                        <div class="mdc-list-item mdc-drawer-item">
                         <a class="mdc-drawer-link" onclick="switchContentTo('dashboard')">
-                            <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">home</i>
-                            Dashboard
-                        </a>
-                    </div>
-                    <div class="mdc-list-item mdc-drawer-item">
+                                <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">home</i>
+                                Dashboard
+                            </a>
+                        </div>
+                        <div class="mdc-list-item mdc-drawer-item">
                         <a class="mdc-drawer-link" onclick="switchContentTo('projects')">
-                            <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">grid_on</i>
-                            Projekte
-                        </a>
-                    </div>
-                    <div class="mdc-list-item mdc-drawer-item">
+                                <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">grid_on</i>
+                                Projekte
+                            </a>
+                        </div>
+                        <div class="mdc-list-item mdc-drawer-item">
                         <a class="mdc-drawer-link" onclick="switchContentTo('employees')">
-                            <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">person</i>
-                            Mitarbeiter
-                        </a>
-                    </div>
-                    <div class="mdc-list-item mdc-drawer-item">
+                                <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">person</i>
+                                Mitarbeiter
+                            </a>
+                        </div>
+                        <div class="mdc-list-item mdc-drawer-item">
                         <a class="mdc-drawer-link" onclick="switchContentTo('statistics')">
-                            <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">pie_chart_outlined</i>
-                            Statistiken
-                        </a>
-                    </div>
-                </nav>
-            </div>
-            <div class="profile-actions">
-                <a href="/">Mitarbeiteransicht</a>
-                <span class="divider"></span>
-                <a href="javascript:;">Logout</a>
-            </div>
+                                <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">pie_chart_outlined</i>
+                                Statistiken
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+                <div class="profile-actions">
+                    <a href="/">Mitarbeiteransicht</a>
+                    <span class="divider"></span>
+                    <form method="post">
+                        <a type="submit" class="anmeldung_form" id="logout" name="logout" onclick="location.href='login.php'">Abmelden</a>
+                    </form>
+                </div>
 
-        </div>
-    </aside>
-    <!-- partial -->
-    <div class="main-wrapper mdc-drawer-app-content">
-        <!-- partial:../../partials/_navbar.html -->
-        <header class="mdc-top-app-bar">
-            <div class="mdc-top-app-bar__row">
-                <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-                    <span class="mdc-top-app-bar__title">Willkommen Laurin Noel !</span>
-                    <div class="mdc-text-field mdc-text-field--outlined mdc-text-field--with-leading-icon search-text-field d-none d-md-flex">
-                        <i class="material-icons mdc-text-field__icon">search</i>
-                        <input class="mdc-text-field__input" id="text-field-hero-input">
-                        <div class="mdc-notched-outline">
-                            <div class="mdc-notched-outline__leading"></div>
-                            <div class="mdc-notched-outline__notch">
-                                <label for="text-field-hero-input" class="mdc-floating-label">Search..</label>
+            </div>
+        </aside>
+        <!-- partial -->
+        <div class="main-wrapper mdc-drawer-app-content">
+            <!-- partial:../../partials/_navbar.html -->
+            <header class="mdc-top-app-bar">
+                <div class="mdc-top-app-bar__row">
+                    <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+                        <span class="mdc-top-app-bar__title">
+                            <div id="angemeldet_als">
+
+                                <?php if (isset($_SESSION['angemeldet']) && $_SESSION['angemeldet'] === true) {
+                                ?>
+                                    <div id="angemeldet_als"><?php echo $benutzerFirstName . ' ' . $benutzerLastName; ?></div>
+                                <?php } else { ?>
+                                <?php } ?>
+
                             </div>
-                            <div class="mdc-notched-outline__trailing"></div>
+                        </span>
+                        <div class="mdc-text-field mdc-text-field--outlined mdc-text-field--with-leading-icon search-text-field d-none d-md-flex">
+                            <i class="material-icons mdc-text-field__icon">search</i>
+                            <input class="mdc-text-field__input" id="text-field-hero-input">
+                            <div class="mdc-notched-outline">
+                                <div class="mdc-notched-outline__leading"></div>
+                                <div class="mdc-notched-outline__notch">
+                                    <label for="text-field-hero-input" class="mdc-floating-label">Search..</label>
+                                </div>
+                                <div class="mdc-notched-outline__trailing"></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end mdc-top-app-bar__section-right">
+                    <div class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end mdc-top-app-bar__section-right">
 
-                    <button onclick="window.location='/';" class="mdc-layout-grid__cell mdc-button mdc-button--raised mdc-button--dense filled-button--light text-small mx-auto">
-                         <i class="material-icons">person</i>
-                        <span class="ml-1">Mitarbeiteransicht</span>
-                    </button>
-                    <span class="divider"></span>
-                    <button class="mdc-layout-grid__cell mdc-button mdc-button--raised mdc-button--dense filled-button--light text-small mx-auto">
-                        <i class="material-icons">logout</i>
-                        <span class="ml-1">Logout</span>
-                    </button>
+                        <button onclick="window.location='/';" class="mdc-layout-grid__cell mdc-button mdc-button--raised mdc-button--dense filled-button--light text-small mx-auto">
+                            <i class="material-icons">person</i>
+                            <span class="ml-1">Mitarbeiteransicht</span>
+                        </button>
+                        <span class="divider"></span>
+                        <form method="post" style="display: inline;">
+                            <button type="submit" class="mdc-layout-grid__cell mdc-button mdc-button--raised mdc-button--dense filled-button--light text-small mx-auto anmeldung_form" id="logout" name="logout">
+                                <i class="material-icons">logout</i>
+                                <span class="ml-1">Logout</span>
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </header>
-        <!-- partial -->
-        <div class="page-wrapper mdc-toolbar-fixed-adjust">
+            </header>
+            <!-- partial -->
+            <div class="page-wrapper mdc-toolbar-fixed-adjust">
             <main class="content-wrapper" id="main">
                 <?php echo $tabContent; ?>
             </main>
+            </div>
         </div>
     </div>
-</div>
 
 
 
 
 
-<!-- plugins:js -->
-<script src="../assets/vendors/js/vendor.bundle.base.js"></script>
-
-<!-- endinject -->
-<!-- Plugin js for this page-->
-<script src="../assets/vendors/chartjs/Chart.min.js"></script>
-<!-- End plugin js for this page-->
-<!-- inject:js -->
-<script src="../assets/js/material.js"></script>
-<script src="../assets/js/misc.js"></script>
-
-<!-- endinject -->
-<!-- Custom js for this page-->
+    <!-- plugins:js -->
+    <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="../assets/js/material.js"></script>
+    <script src="../assets/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
 <script>
     var currentTab = "<?php echo $currentTab; ?>";
 </script>
-
-<!-- End custom js for this page-->
+    <!-- End custom js for this page-->
 </body>
+
 </html>
