4543fb33f1bc4f765f1da115f9e2c82aa097a244
saka <laurinfrank03@gmail.com>
2024-07-01 09:24:53 +0200

    created npm project with tailwind.config.js and postcss.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#F7CD45',
+        'secondary': '#0F2D3B',
+        'normal': '#DCE2E5',
+      }
+    },
+  },
+  plugins: [],
+}
+
