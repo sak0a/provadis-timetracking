0572e8fafe9d73a28f193f5824a54dc96ceb68b6
Anton <klimmster1@gmail.com>
2024-07-03 14:28:19 +0200

    Es wurden all_projects_details.php API ghinzufügt für Statistiken

--- admin/statistics.php
+++ admin/statistics.php
@@ -13,44 +13,26 @@ if (strpos($_SERVER['REQUEST_URI'], 'admin/statistics.php') !== false) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>CommerzBau</title>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../dist/css/style.purged.css">
+    <link rel="stylesheet" href="../dist/css/global.css">
+    <link rel="stylesheet" href="../dist/css/admin.css">
   <link rel="shortcut icon" href="../assets/images/favicon.png" />
-    <script src="../assets/js/chartjs.js"></script>
+ 
+     
 </head>
 
 <body>
-
-  <div class="container">
-
-    <div class="section">
-      <h2>Statistik</h2>
+<div class="projects-container">
+    <!-- Benutzerübersicht -->
+    <div class="projects-main-container">
+        <!-- Container Header -->
+        <!-- <div class="mdc-layout-grid__inner"> -->
+        <div class="statistic_container" id="projectDetailsContent">
+        </div>
+      <!-- </div> -->
     </div>
+</div>
 
-
-
-    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6-desktop">
-      <div class="mdc-card">
-        <h6 class="card-title">Pie chart</h6>
-        <canvas id="pieChart"></canvas>
-
-      </div>
-    </div>
-
-  </div>
-  <!-- partial -->
-  <!-- plugins:js -->
-  <script src="../assets/vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="../assets/vendors/chartjs/Chart.min.js"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="../../assets/js/chartjs.js"></script>
-  <!-- End custom js for this page-->
+   
 </body>
-
 </html>
---
+++ assets/js/admin/statistics.js
@@ -0,0 +1,100 @@
+    fetch(`all_projects_details.php`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                console.error(data.error);
+            } else {
+                let detailsDiv = document.getElementById('projectDetailsContent');
+                detailsDiv.innerHTML = data.map(project => `
+                        <div class="statistic_box">
+                        <br>
+                        <details open><summary><h2>Projektdetails für ${project['Projektname']}</h2></summary>
+                        <p><table class="details-table">
+                            <tr>
+                                <th>Attribut</th>
+                                <th>Wert</th>
+                            </tr>
+                            <tr>
+                                <td>Projekt-ID</td>
+                                <td>${project['Projekt-ID']}</td>
+                            </tr>
+                            <tr>
+                                <td>Projektleiter</td>
+                                <td>${project['Projektleiter']}</td>
+                            </tr>
+                            <tr>
+                                <td>Startdatum</td>
+                                <td>${project['Startdatum']}</td>
+                            </tr>
+                            <tr>
+                                <td>Enddatum</td>
+                                <td>${project['Enddatum']}</td>
+                            </tr>
+                            <tr>
+                                <td>Status</td>
+                                <td>${project['Status']}</td>
+                            </tr>
+                            <tr>
+                                <td>Geplante Zeit</td>
+                                <td>${project['Geplannte Zeit']}</td>
+                            </tr>
+                            <tr>
+                                <td>Gesamte Stunden</td>
+                                <td>${project['Gesamte Stunden']}</td>
+                            </tr>
+                        </table></p>
+                        <div class="mdc-card">
+                            <canvas id="pieChart_${project['Projekt-ID']}"></canvas>
+                        </div>
+                        </details>
+                        </div>
+                `).join('');
+
+                // Lade Chart.js, falls nicht geladen
+                if (!window.Chart) {
+                    const script = document.createElement('script');
+                    script.src = 'https://cdn.jsdelivr.net/npm/chart.js';
+                    script.onload = function() {
+                        createCharts(data);
+                    };
+                    document.head.appendChild(script);
+                } else {
+                    createCharts(data);
+                }
+            }
+        })
+        .catch(error => console.error(error));
+
+function createCharts(data) {
+    data.forEach(project => {
+        const canvas = document.getElementById(`pieChart_${project['Projekt-ID']}`).getContext('2d');
+        const plannedHours = project['Geplannte Zeit'];
+        const totalHours = project['Gesamte Stunden'];
+        const percentHours = (plannedHours / totalHours) * 100;
+        const percentDiff = 100 - percentHours;
+
+        const doughnutPieData = {
+            labels: ['Geplannte Zeit', 'Tatsächliche Zeit'],
+            datasets: [{
+                data: [percentHours, percentDiff],
+                backgroundColor: ['#ff6384', '#36a2eb'],
+                hoverBackgroundColor: ['#ff6384', '#36a2eb']
+            }]
+        };
+
+        const doughnutPieOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        };
+
+        new Chart(canvas, {
+            type: 'pie',
+            data: doughnutPieData,
+            options: doughnutPieOptions
+        });
+    });
+}
